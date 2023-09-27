78ae9c36a09df557b9a01f40cbf854c4364ff037
RodrigoLime <112708096+RodrigoLime@users.noreply.github.com>
2023-09-27 20:19:49 -0300

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -16,6 +16,11 @@ public class BinaryTree extends Main {
 	public BinaryTree(BTNode root) {
 		this.root = root;
 	}
+	
+	public BTNode getRoot() {
+		return root;
+	}
+	
 	public boolean isEmpty() {
 		return root == null;
 	}
@@ -65,55 +70,50 @@ public class BinaryTree extends Main {
 	}
 	
 	//Metodo buildExpressionTree
-	public void buildExpressionTree(String expInfixa) {
-		if(isValidExpression(expInfixa)) {
-			//converter a expressao infixa em pos fixa
-			String expPosfixa = infixaPosfixa(expInfixa);
-			//Criar a arvore a partir da expressao pos-fixa
-			Stack<BTNode> nodeStack = new Stack<>();
-			int index = 0;
-			for (char token : expPosfixa.toCharArray()) {
-				if (isOperator(token)) {
-					//Operador encontrado, criar no operador
-					BTNodeOperador operatorNode = new BTNodeOperador(token);
-					//Pop dois operandos da pilha
-					BTNode operando2= nodeStack.pop();
-					BTNode operando1= nodeStack.pop();
-					//Definir os filhos do no operador
-					operatorNode.setLeft(operando1);
-					operatorNode.setRight(operando2);
-					//Empilhar o no operador
-					nodeStack.push(operatorNode);
-				}
-				else {
-					//Operando encontrado, criar no operando
-					StringBuilder valorStr = new StringBuilder();
-					int startIndex = index;
+	public void buildExpressionTree(String expPosfixa) {
+		//Criar a arvore a partir da expressao pos-fixa
+		Stack<BTNode> nodeStack = new Stack<>();
+		int index = 0;
+		for (char token : expPosfixa.toCharArray()) {
+			if (isOperator(token)) {
+				//Operador encontrado, criar no operador
+				BTNodeOperador operatorNode = new BTNodeOperador(token);
+				//Pop dois operandos da pilha
+				BTNode operando2= nodeStack.pop();
+				BTNode operando1= nodeStack.pop();
+				//Definir os filhos do no operador
+				operatorNode.setLeft(operando1);
+				operatorNode.setRight(operando2);
+				//Empilhar o no operador
+				nodeStack.push(operatorNode);
+				//System.out.print(
+				index++;
+			}
+			else {
+				//Operando encontrado, criar no operando
+				StringBuilder valorStr = new StringBuilder();
 					
-					while(Character.isDigit(token) || token == '.') {
-						valorStr.append(token);
-						index++;
-						if (index < expPosfixa.length()) {
-							token = expPosfixa.charAt(index);
-						}
-						else {
-							break;
-						}
-						
+				while(Character.isDigit(token) || token == '.') {
+					valorStr.append(token);
+					index++;
+					if (index < expPosfixa.length()) {
+						token = expPosfixa.charAt(index);
 					}
-					float valor = Float.parseFloat(valorStr.toString());
-					BTNodeOperando operandNode = new BTNodeOperando(valor);
-					//Empilhar o no operando
-					nodeStack.push(operandNode);
+					else {
+						break;
+					}
+						
+				}
+				float valor = Float.parseFloat(valorStr.toString());
+				BTNodeOperando operandNode = new BTNodeOperando(valor);
+				//Empilhar o no operando
+				nodeStack.push(operandNode);
 
 				}
 			}
-			// O ultimo no na pilha e a raiz da arvore
+			// O ultimo no na pilha eh a raiz da arvore
 			this.root = nodeStack.pop();
-		}
-		else {
-			System.out.println("Expressao invalida");
-		}
+	
 	} //fim da classe buildExpressionTree
 	
 	
