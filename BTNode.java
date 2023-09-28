486404e33ebcc9d02b5187a860c5717320e6cefa
RodrigoLime <112708096+RodrigoLime@users.noreply.github.com>
2023-09-28 01:08:40 -0300

    Update BTNode.java

--- BTNode.java
+++ BTNode.java
@@ -5,67 +5,70 @@ NOME: MATEUS KENZO IOCHIMOTO 						TIA: 32216289
 NOME: RODRIGO MACHADO DE ASSIS OLIVEIRA DE LIMA		TIA: 32234678
 NOME: THIAGO SHIHAN CARDOSO TOMA					TIA: 32210744
 */
-package apl1;
 
 public class BTNode {
+	protected Object data;
 	private BTNode parent;
 	private BTNode left;
 	private BTNode right;
 	
 	public BTNode() {
-		parent = null;
-		left = null;
-		right = null;
+		this(' ', null);
 	}
-	
-	public BTNode(BTNode parent) {
+	public BTNode(Object data) {
+		this(data, null);
+	}
+	public BTNode(Object data, BTNode parent) {
+		this.data = data;
 		this.parent = parent;
 		this.left = null;
 		this.right = null;
 	}
-	
+	public Object getData() {
+        if (data instanceof Character) {
+            return (char) data;
+        } else if (data instanceof Float) {
+            return (float) data;
+        }
+        // Outros casos aqui, se necessário.
+        return data;
+    }
+
+	public void setData(char data) {
+		this.data = data;
+	}
 	public BTNode getParent() {
 		return parent;
 	}
-	
 	public void setParent(BTNode parent) {
 		this.parent = parent;
 	}
-	
 	public BTNode getLeft() {
 		return left;
 	}
-	
 	public void setLeft(BTNode left) {
 		this.left = left;
 		left.setParent(this);
 	}
-	
 	public BTNode getRight() {
 		return right;
 	}
-	
 	public void setRight(BTNode right) {
 		this.right = right;
 		right.setParent(this);
 	}
-	
 	public boolean hasLeftChild() {
 		return left != null;
 	}
-	
 	public boolean hasRightChild() {
 		return right != null;
 	}
-	
 	public boolean isRoot() {
 		return parent == null;
 	}
-	
 	public boolean isLeaf() {
 		return left == null && right == null;
 	}
-	
 	public int getDegree() {
 		int degree = 0;
 		if (hasLeftChild())
@@ -74,14 +77,12 @@ public class BTNode {
 			++degree;
 		return degree;
 	}
-	
 	public int getLevel() {
 		if(isRoot()) {
 			return 0;
 		}
 		return parent.getLevel() + 1;
 	}
-	
 	public int getHeight() {
 		if(isLeaf()) {
 			return 0;
@@ -94,23 +95,18 @@ public class BTNode {
 		return height + 1;
 	}
 	
+	
 	public float visitar() {
 		return Float.NaN;
 	}
 	
-	public static boolean isOperator(char ch) {
-		return ch == '+' || ch == '-' || ch == '*' || ch == '/';
-	}
-	
-	public static boolean isOperand(char ch) {
-		return Character.isDigit(ch) || ch == '.';	//considera ponto como parte de operandos validos (numeros decimais)
-	}
-	
+
 	@Override
 	public String toString() {
-		return "parent: " + (parent != null ? parent.visitar() : "null")
-				+ ", left: " + (left != null ? left.visitar() : "null")
-				+ ", right: " + (right != null ? right.visitar(): "null")
+		return "data: " + data
+				+ ", parent: " + (parent != null ? parent.getData() : "null")
+				+ ", left: " + (left != null ? left.getData() : "null")
+				+ ", right: " + (right != null ? right.getData(): "null")
 				+ ", isRoot()" + isRoot()
 				+ ", isLeaf(): " + isLeaf()
 				+ ", getDegree(): " + getDegree()
