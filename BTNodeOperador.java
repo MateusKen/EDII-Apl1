e23e8f190b9ad355c9a1180e470989bab0c93762
RodrigoLime <112708096+RodrigoLime@users.noreply.github.com>
2023-09-28 01:09:28 -0300

    Update BTNodeOperador.java

--- BTNodeOperador.java
+++ BTNodeOperador.java
@@ -5,27 +5,37 @@ NOME: MATEUS KENZO IOCHIMOTO 						TIA: 32216289
 NOME: RODRIGO MACHADO DE ASSIS OLIVEIRA DE LIMA		TIA: 32234678
 NOME: THIAGO SHIHAN CARDOSO TOMA					TIA: 32210744
 */
-package apl1;
 
 public class BTNodeOperador extends BTNode{
-	private char operador;
 	
-	public BTNodeOperador(char operador) {
-		super();
-		this.operador = operador;
+	public BTNodeOperador(char data) {
+		super(data);
 	}
 	
-	public BTNodeOperador(BTNode parent, char operador) {
-		super(parent);
-		this.operador = operador;
-	}
 	
-	public void setOperador(char operador) {
-		this.operador = operador;
-	}
 	
 	@Override
 	public float visitar() {
-		return operador;
+		float valorEsq = this.getLeft().visitar();
+		float valorDir = this.getRight().visitar();
+		
+		switch ((char)getData()) {
+		case '+':
+			return (valorEsq + valorDir);
+		case '-':
+			return valorEsq - valorDir;
+		case '*': 
+			return valorEsq * valorDir;
+		case '/':
+			if (valorDir != 0) {
+				return valorEsq / valorDir;
+			}
+			else {
+				System.out.println("Divisao por zero!");
+				return Float.NaN;
+			}
+
+		}
+		return Float.NaN;
 	}
 }
