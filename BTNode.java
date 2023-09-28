15d8e8698da5b614d96e9e1df8517bfc815151a9
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-09-27 21:00:17 -0300

    Update BTNode.java

--- BTNode.java
+++ BTNode.java
@@ -5,63 +5,61 @@ NOME: MATEUS KENZO IOCHIMOTO 						TIA: 32216289
 NOME: RODRIGO MACHADO DE ASSIS OLIVEIRA DE LIMA		TIA: 32234678
 NOME: THIAGO SHIHAN CARDOSO TOMA					TIA: 32210744
 */
+package apl1;
 
 public class BTNode {
-	private char data;
 	private BTNode parent;
 	private BTNode left;
 	private BTNode right;
 	
-	public BTNode() {
-		this(' ', null);
-	}
-	public BTNode(char data) {
-		this(data, null);
-	}
-	public BTNode(char data, BTNode parent) {
-		this.data = data;
+	public BTNode(BTNode parent) {
 		this.parent = parent;
 		this.left = null;
 		this.right = null;
 	}
-	public char getData() {
-		return data;
-	}
-	public void setData(char data) {
-		this.data = data;
-	}
+	
 	public BTNode getParent() {
 		return parent;
 	}
+	
 	public void setParent(BTNode parent) {
 		this.parent = parent;
 	}
+	
 	public BTNode getLeft() {
 		return left;
 	}
+	
 	public void setLeft(BTNode left) {
 		this.left = left;
 		left.setParent(this);
 	}
+	
 	public BTNode getRight() {
 		return right;
 	}
+	
 	public void setRight(BTNode right) {
 		this.right = right;
 		right.setParent(this);
 	}
+	
 	public boolean hasLeftChild() {
 		return left != null;
 	}
+	
 	public boolean hasRightChild() {
 		return right != null;
 	}
+	
 	public boolean isRoot() {
 		return parent == null;
 	}
+	
 	public boolean isLeaf() {
 		return left == null && right == null;
 	}
+	
 	public int getDegree() {
 		int degree = 0;
 		if (hasLeftChild())
@@ -70,12 +68,14 @@ public class BTNode {
 			++degree;
 		return degree;
 	}
+	
 	public int getLevel() {
 		if(isRoot()) {
 			return 0;
 		}
 		return parent.getLevel() + 1;
 	}
+	
 	public int getHeight() {
 		if(isLeaf()) {
 			return 0;
@@ -88,7 +88,6 @@ public class BTNode {
 		return height + 1;
 	}
 	
-	
 	public float visitar() {
 		return Float.NaN;
 	}
@@ -100,10 +99,10 @@ public class BTNode {
 	public static boolean isOperand(char ch) {
 		return Character.isDigit(ch) || ch == '.';	//considera ponto como parte de operandos validos (numeros decimais)
 	}
+	
 	@Override
 	public String toString() {
-		return "data: " + data
-				+ ", parent: " + (parent != null ? parent.getData() : "null")
+		return ", parent: " + (parent != null ? parent.getData() : "null")
 				+ ", left: " + (left != null ? left.getData() : "null")
 				+ ", right: " + (right != null ? right.getData(): "null")
 				+ ", isRoot()" + isRoot()
